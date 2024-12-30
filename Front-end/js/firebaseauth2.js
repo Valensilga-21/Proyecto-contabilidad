29dc66616ed8a67d9f8a24125cdc2c4fef41f9ee
Valentina Silva <valesilga21@gmail.com>
2024-12-30 17:52:04 -0500

    Login funciona y se añadieron alertas mas manual de usuario

---
+++ Front-end/js/firebaseauth2.js
@@ -0,0 +1,58 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {getAuth, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCKTJxwwP2roq8DioEyhrBWMNN34f2JB6Y",
+    authDomain: "lcd-contabilidad.firebaseapp.com",
+    projectId: "lcd-contabilidad",
+    storageBucket: "lcd-contabilidad.firebasestorage.app",
+    messagingSenderId: "289220346007",
+    appId: "1:289220346007:web:b9034c2734e3602830a9d2",
+    measurementId: "G-2LDHTHRHNY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const signIn=document.getElementById('submitSignIn');
+signIn.addEventListener('click', (event)=>{
+   event.preventDefault();
+   const email=document.getElementById('correo_usuario').value;
+   const password=document.getElementById('contrasena').value;
+   const auth=getAuth();
+
+   signInWithEmailAndPassword(auth, email, password)
+
+   Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Your work has been saved",
+    showConfirmButton: false,
+    timer: 2500
+  })
+   .then((userCredential)=>{
+       const user=userCredential.user;
+       localStorage.setItem('loggedInUserId', user.uid);
+       window.location.href='inicio.html';
+   })
+   .catch((error)=>{
+       const errorCode=error.code;
+       if(errorCode==='auth/invalid-credential'){
+        Swal.fire({
+            title: "!Oops!",
+            text: "Correo o contraseña incorrectos",
+            icon: "error",
+            confirmButtonColor: "#ff0000"
+        })
+       }
+       else{
+        Swal.fire({
+            title: "!Oops!",
+            text: "Usuario no encontrado",
+            icon: "error",
+            confirmButtonColor: "#ff0000"
+        })
+       }
+   })
+});
